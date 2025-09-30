5e6125ac5176dce0f306d478040def7d5abb185f
londonreturns <subedioscar.47@gmail.com>
2025-09-30 17:09:09 +0545

    basic main page finished

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="flex w-full max-w-sm items-center gap-2">
+        <Input type="string" placeholder="Enter here" />
+        <Button type="submit" variant="default">
+          Proceed
+        </Button>
+      </div>
+    </div>
+  );
 }
